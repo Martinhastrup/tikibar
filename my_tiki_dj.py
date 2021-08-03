4955957f47a86ff24607ebde4f7dd7e917b30738
Martin Hastrup <Martin Hastrup>
2021-08-03 17:59:53 +0200

    changing player

--- my_tiki_dj.py
+++ my_tiki_dj.py
@@ -9,16 +9,22 @@ Created on Sun Aug  1 20:52:27 2021
 
 import os
 import random
-from playsound import playsound
+#from playsound import playsound
+import pygame
 
 
 if __name__ == "__main__":
     # Take me to Kokomo
     print('takeoff')
+    pygame.mixer.init()
     my_tracklist = os.listdir('tracklist')
     while True:
         random.shuffle(my_tracklist)
         for hit in my_tracklist:
             print('Now playing: {}'.format(hit))
-            playsound('tracklist/{}'.format(hit))
+            #playsound('tracklist/{}'.format(hit))
+            pygame.mixer.music.load('tracklist/{}'.format(hit))
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                continue
         
